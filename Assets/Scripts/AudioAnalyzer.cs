44a2b8f19e85c608989d6a6a5b5f42342dba58ee
malo <malo@fabric.ch>
2018-02-09 13:59:03 +0100

    basic sound viewer

---
+++ Assets/Scripts/AudioAnalyzer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class AudioAnalyzer : MonoBehaviour {
+
+    AudioSource audioSource = null;
+    public float[] samples;
+    public int samplesSize = 512;
+
+    public Transform dancerPrefabT = null;
+    public Dancer[] dancers;  
+
+	// Use this for initialization
+	void Start () {
+
+        audioSource = this.GetComponent<AudioSource>();
+        samples = new float[samplesSize];
+        dancers = new Dancer[samplesSize];
+
+        Dancer dancerPrefab = dancerPrefabT.GetComponent<Dancer>();
+        if( dancerPrefab == null)
+        {
+            Debug.LogError("AUDIOANALYZER: dancerPrefabT does not contains Dancer script!");
+        }
+
+        Vector3 tempPostion = Vector3.zero;
+        Vector3 increment = new Vector3(dancerPrefab.GetWidth(), dancerPrefab.GetHeight()/2f , 0);
+        for( int i = 0; i < samplesSize; i++)
+        {
+            dancers[i] = CreateDancer(tempPostion);
+            tempPostion += increment;
+        }
+
+    }
+	
+    private Dancer CreateDancer(Vector3 position)
+    {
+        Transform newDancerT = Instantiate(dancerPrefabT);
+        newDancerT.SetParent(this.transform);
+        newDancerT.transform.position = position;
+
+        return newDancerT.GetComponent<Dancer>(); 
+    }
+
+	// Update is called once per frame
+	void Update () {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+
+      
+        for( int i = 0; i < samplesSize; i++)
+        {
+            dancers[i].SetHeight(samples[i] * 10 + 0.1f); 
+        }
+	}
+    
+}
---
+++ Assets/Scripts/Dancer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dancer : MonoBehaviour {
+
+
+    private float width = 0.1f;
+    private float height = 0.1f;
+    private float deep = 0.01f;
+
+
+    public float GetWidth()
+    {
+        return width;
+    }
+
+
+    public void SetWidth(float newWidth)
+    {
+        this.width = newWidth;
+        Vector3 newLocalScale = this.transform.localScale;
+        newLocalScale.x = width;
+        this.transform.localScale = newLocalScale;
+
+    }
+
+    public float GetDeep() {
+        return deep;
+    }
+
+
+    public void SetDeep(float newDeep)
+    {
+        this.deep = newDeep;
+        Vector3 newLocalScale = this.transform.localScale;
+        newLocalScale.z = deep;
+        this.transform.localScale = newLocalScale;
+
+    }
+
+
+    public void SetHeight(float newHeight)
+    {
+        height = newHeight;
+        Vector3 newLocalScale = this.transform.localScale;
+        newLocalScale.y = height;
+        this.transform.localScale = newLocalScale;
+        Vector3 newPosition = this.transform.position;
+        newPosition.y = height / 2f;
+        this.transform.position = newPosition; 
+    }
+
+    public float GetHeight()
+    {
+        return height;
+    }
+
+	// Use this for initialization
+	void Start () {
+        SetWidth(this.width);
+        SetDeep(deep); 
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
