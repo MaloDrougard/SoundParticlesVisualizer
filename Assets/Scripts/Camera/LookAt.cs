2ab7d6fd0fda957cc3abadfe9bcfab819cd90233
MaloDrougard <malcommalo.drougard@epfl.ch>
2019-02-02 11:29:52 +0100

    Add GLU license

--- Assets/Scripts/Camera/LookAt.cs
+++ Assets/Scripts/Camera/LookAt.cs
@@ -1,3 +1,26 @@
+/*
+* Copyright (C) 2017-2019 Makem Corporation 
+* 
+* Created: 2018 Malo Drougard <malo.drougard@protonmail.com>
+* 
+* This file is part of SoundParticlesVisualizer (SPV).
+* 
+* SPV is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* SPV is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with SPV.  If not, see <https://www.gnu.org/licenses/>.
+*
+*/
+
+
 ﻿//using System.Collections;
 //using System.Collections.Generic;
 //using UnityEngine;
